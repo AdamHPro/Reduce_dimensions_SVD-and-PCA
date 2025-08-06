1ee625f976276fc62b4585af2ae63e85dc736497
Hachicha Adam <adam.hachicha@student-cs.fr>
2025-08-06 15:01:29 +0100

    fonctionnalité produit ajouté, et petite modif graphique

--- comparison.py
+++ comparison.py
@@ -4,4 +4,5 @@ def qui_est_grand(a, b) :
     elif a>b :
         print(a + "est strictement plus grand que" + b)
     else :
-        print( a + "=" + b)
+        print( a + "est égal à" + b)
+    print("Voici en plus le produit :"+a*b)
