45c135beb408bec54b14cdd27a749e9a8b93e9ab
Hachicha Adam <adam.hachicha@student-cs.fr>
2025-08-06 14:16:27 +0100

    More precise comparison function

--- comparison.py
+++ comparison.py
@@ -1,5 +1,7 @@
 def qui_est_grand(a, b) :
-    if a<=b :
-        print(b + "est plus grand que" + a)
+    if a<b :
+        print(b + "est strictement plus grand que" + a)
+    elif a>b :
+        print(a + "est strictement plus grand que" + b)
     else :
-        print(a + "est plus grand que" + b)
+        print( a + "est égal à" + b)
